73c67ae766a1272cb2a6ecf60c25e234376174e9
Amanda King <amapickell@gmail.com>
2019-08-17 18:08:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,10 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
-    doc = 
+    doc = Nokogiri::HTML(html)
+    profiles = {}
+    
+    
   end
 
   def self.scrape_profile_page(profile_url)
